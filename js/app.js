dd3f73e5eee13b96666b973780c903c7bbbe6d5d
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-29 12:58:48 +0100

    add more else if > number

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
 // Create a Guessing game. Let users guess a number.
 
-const number = 55;
+const number = 100;
 
-let guessingGame = ParseInt(prompt('guess a number'));
-
-if ( guessingGame === number){
-    alert('Gracious, Obrigados gringo !!!');
+let guessNum = (prompt("guess a number"));
+// if statement of the correct number
+if ( guessNum === 100){
+    alert("Gracious, Obrigados gringo !!!")
+}
+// elseif of the wrong number
+else if (guessNum < number){
+    alert("try again mate: the number is greater than" +   " the number you guessed mate !")
+    
+}
+// another else if > number
+else if(guessNum > number){
+    alert("No way this is outragious mate LOL ! come down")
+    
 }
 
-elseif('')
+// else for none of both guessingNumber nor if or elseif
+else {
+    alert("You should think of a valid number")
+}
