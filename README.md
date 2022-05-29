eab73e2664a7aad6d4effb501de11af3d5f4b732
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-29 14:30:38 +0100

    write table of content and more decsription on Readme file

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # guessing game
 
 *this game is about guessing the right number
-*while there will be a prompt if correct, less than the number and greater than the number
+*while there will be a prompt if correct, less than the number and greater than the 
 
-Copyright of Acedu
+>Table of content
+.Write an if statement
+.Write else if statement
+.Write else statement
+.declear a variable
+.declear a constant number
+
+
+#List  
+1. html
+2. javascript
+3. css
+
+
+Copyright of Adeyemi B.A
--- js/app.js
+++ js/app.js
@@ -8,6 +8,18 @@ let guessNum = parseInt(prompt("Let see how smart you are mate. Now guess the ri
 if (guessNum ===number){
     alert("Gracious, Obrigados gringo !!!")
 }
+
+// else if Not a number (NAN)
+else if(guessNum =NaN){
+    alert("Kindly guess an actual number not a word !")
+}
+
+// // else if guessNum greater than 800 and less than 9000
+// else if( guessNum => 800 && guessNum <= 9000){
+//     alert("Bro Chill we are not in ukraine")
+// }
+
+
 // elseif < number
 else if (guessNum < number){
     alert("try again mate: the number is greater than" +   " the number you guessed mate !")
@@ -18,10 +30,6 @@ else if(guessNum > number){
     alert("No way this is outragious mate LOL ! come down")
     
 }
-
-// else if( guessNum => 800 && guessNum <= 9000){
-//     alert("Bro Chill we are not in ukrain")
-// }
 
 // else for none of both guessingNumber nor if or elseif
 else {
