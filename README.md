b021b3ef78e7d3d7d7a4e2e4f10ea7e70923543d
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-29 13:29:12 +0100

    add the description of the game in README file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # guessing game
+
+*this game is about guessing the right number
+*while there will be a prompt if correct, less than the number and greater than the number
+
+Copyright of Acedu
--- js/app.js
+++ js/app.js
@@ -13,11 +13,15 @@ else if (guessNum < number){
     alert("try again mate: the number is greater than" +   " the number you guessed mate !")
     
 }
-// another else > number
+// another  else if > number
 else if(guessNum > number){
     alert("No way this is outragious mate LOL ! come down")
     
 }
+
+// else if( guessNum => 800 && guessNum <= 9000){
+//     alert("Bro Chill we are not in ukrain")
+// }
 
 // else for none of both guessingNumber nor if or elseif
 else {
