1c748e04f21f3a6c612fe36eab90134ea16fe184
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-29 12:18:13 +0100

    create a guessing game with if statement

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Create a Guessing game. Let users guess a number.
+
+const number = 55;
+
+let guessingGame = ParseInt(prompt('guess a number'));
+
+if ( guessingGame === number){
+    alert('Gracious, Obrigados gringo !!!');
+}
+
+elseif('')
